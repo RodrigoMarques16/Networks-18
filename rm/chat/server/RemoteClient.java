9af9ee80f319c6880b8215c6bbfe278392b57a57
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-11-11 01:07:22 +0000

    finalized RemoteClient

--- rm/chat/server/RemoteClient.java
+++ rm/chat/server/RemoteClient.java
@@ -1,10 +1,61 @@
 package rm.chat.server;
 
+/**
+ * A remote instance of a client. An instance of this class records a player's
+ * ID, state and which room the client is in, if any.
+ */
 public class RemoteClient {
+
+    /**
+     * States a Client can be in.
+     */
+    public static enum State {
+        /**
+         * User is registered but not inside a room.
+         */
+        INIT,
+
+        /**
+         * User is registered but not inside a room.
+         */
+        OUTSIDE,
+
+        /**
+         * User is currently inside a room.
+         */
+        INSIDE
+    }
+
+    private static int ID;
+
     private State state;
     private int room;
     private String nick;
     private int id;
+
+    /**
+     * Create an instance of a RemoteClient and give it a name Stat initialized to
+     * INIT.
+     * 
+     * @see State#INIT
+     */
+    public RemoteClient() {
+        this.id = getNewId();
+        this.state = INIT;
+    }
+
+    /**
+     * Create an instance of a RemoteClient and give it a name State is initialized
+     * to OUTSIDE.
+     * 
+     * @see State#OUTSIDE
+     * @param nick Client's username
+     */
+    public RemoteClient(String nick) {
+        this.id = ++ID;
+        this.nick = nick;
+        this.state = OUTSIDE;
+    }
 
     public int getState() {
         return this.state;
@@ -28,14 +79,14 @@ public class RemoteClient {
 
     setNick(String nick) {
         this.nick = nick;
-        // broadcast
     }
 
     public int getId() {
         return this.id;
     }
 
-    setId(int id) {
-        this.id = id;
+    private int getNewId() {
+        return ++ID;
     }
+
 }
